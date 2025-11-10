3ef07ff211e1bf2558bd930c55f1263f7a339613
gussakah00 <aditia.2308561012@student.unud.ac.id>
2025-11-11 03:21:29 +0800

    Initial commit: Cerita di Sekitarmu PWA

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
-    publicPath: "berhasil/",
+    publicPath: "revisi2/",
     clean: true,
   },
   optimization: {
